5d36fd151345fa67dd699c4cc0966f692caf4d98
Victor Szeto <v.victorszeto@gmail.com>
2022-12-17 16:42:59 -0500

    first post

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,8 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
+  { href: '/posts', title: 'Posts' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  // { href: '/projects', title: 'Projects' },
+  // { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
